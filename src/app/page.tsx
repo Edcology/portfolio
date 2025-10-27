8adfb4758bbdffade4ad35fe31b8f0483fe003f6
Awofala Gbolahan <edawofal@gmail.com>
2025-10-27 15:24:04 +0100

    Added the header component

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex flex-col justify-center bg-linear-to-br from-white to-gray-100 py-6 px-4 min-h-screen'>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid"
+import Link from "next/link"
+import { useState } from "react"
+
+const Header = () => {
+    const [clicked, setClicked] = useState(false)
+  return (
+    <div className="fixed top-6 left-0 right-0 px-3">
+        <nav className="mx-auto bg-black/10 backdrop-blur-md border border-white/20 rounded-4xl p-4 md:px-6 shadow-sm w-full max-w-5xl">
+            <div className="flex justify-between">
+                <div>
+                    <h1 className="text-xl">&lt;Awofala Gbolahan/&gt;</h1>
+                </div>
+                <div className="md:hidden cursor-pointer transition transit" onClick={() => setClicked(!clicked)}>
+                    {clicked ? <XMarkIcon className="h-6 w-6 transition-transform duration-300 rotate-90"/> : <Bars3Icon className="h-6 w-6 transition-transform duration-300"/>}
+                </div>
+                <div className="hidden md:block">
+                    <ul className="flex gap-3">
+                        <Link href='#home'><li>Home</li></Link>
+                        <Link href='#about'><li>About</li></Link>
+                        <Link href='#skills'><li>Skills</li></Link>
+                        <Link href='#projects'><li>Projects</li></Link>
+                        <Link href='#experience'><li>Experience</li></Link>
+                        <Link href='#contact'><li>Contact</li></Link>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        {
+            clicked && (
+                <div className="mt-4 text-lg">
+                   <ul className="flex flex-col gap-2 px-3 border-b border-b-black">
+                        <Link className="hover:underline" href='#home'><li>Home</li></Link>
+                        <Link className="hover:underline" href='#about'><li>About</li></Link>
+                        <Link className="hover:underline" href='#skills'><li>Skills</li></Link>
+                        <Link className="hover:underline" href='#projects'><li>Projects</li></Link>
+                        <Link className="hover:underline" href='#experience'><li>Experience</li></Link>
+                        <Link className="hover:underline" href='#contact'><li>Contact</li></Link>
+                    </ul> 
+                </div>
+            )
+        }
+    </div>
+  )
+}
+
+export default Header
