83d5c79b1df0febb63a81f1bed58c2d13647684b
Awofala Gbolahan <edawofal@gmail.com>
2025-10-30 23:47:31 +0100

    Added animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,45 @@
-import About from '@/components/About'
-import Contact from '@/components/Contact'
-import Experience from '@/components/Experience'
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import Projects from '@/components/Projects'
-import Skills from '@/components/Skills'
+import About from '@/components/About';
+import Contact from '@/components/Contact';
+import Experience from '@/components/Experience';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
+import Reveal from '@/components/Reveal';
+import Skills from '@/components/Skills';
 
 const Home = () => {
   return (
-    <div className='flex flex-col justify-center py-6 min-h-screen'>
+    <main className='flex flex-col justify-center py-6 space-y-24 min-h-screen'>
       <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Experience />
-      <Contact />
-      <Footer />
-    </div>
-  )
-}
+      
+      <Reveal>
+        <Hero />
+      </Reveal>
 
-export default Home
+      <Reveal>
+        <About />
+      </Reveal>
+
+      <Reveal>
+        <Skills />
+      </Reveal>
+
+      <Reveal>
+        <Projects />
+      </Reveal>
+
+      <Reveal>
+        <Experience />
+      </Reveal>
+
+      <Reveal>
+        <Contact />
+      </Reveal>
+
+      <Footer />
+    </main>
+  );
+};
+
+export default Home;
